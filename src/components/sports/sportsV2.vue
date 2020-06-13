9de62d40f0df335df85ac433f403f94e8442d8ce
quyanjie <quyanjie1991.com>
2020-06-13 17:02:26 +0800

    模拟冲突

--- src/components/sports/sportsV2.vue
+++ src/components/sports/sportsV2.vue
@@ -4,76 +4,79 @@
     <h2 class="sub-header">Sports Charts V2</h2>
     <div>
       <select>
-        <option ></option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
       </select>
     </div>
     <div id="echartsDiv" style="height: 760px;width: 100%"></div>
+    <button>hello world</button>
   </div>
 </template>
 <script>
-  //引入axios,表格需要数据，所以在list中引用
-  // import axios from "axios";
+//引入axios,表格需要数据，所以在list中引用
+// import axios from "axios";
 
-  export default {
-    data() {
-      return {
-        list: [],
-        myChart: null
-      };
-    },
-    methods: {
-      draw() {
-        console.log("abc上");
-        this.myChart = this.$echarts.init(document.getElementById("echartsDiv"));
-        console.log("abc下");
-        // console.log('myChart: ', myChart)
-        // 指定图表的配置项和数据
-        let dates = new Array()
-        let steps = new Array()
-        for (let i = 0; i < this.list.length; i++) {
-          dates.push(this.list[i].happenDate)
-          steps.push(this.list[i].reachAmount)
-        }
-        console.log('dates', dates)
-        console.log('steps', steps)
-        var option = {
-          xAxis: {
-            type: "category",
-            data: dates
-          },
-          yAxis: {
-            type: "value"
-          },
-          series: [
-            {
-              data: steps,
-              type: "bar"
-            }
-          ]
-        };
-
-        // 使用刚指定的配置项和数据显示图表。
-        this.myChart.setOption(option, true);
-        console.log("setOption(option)下");
-        console.log("option:", option);
-      },
-      getAllList() {
-        this.$http.get("matter_log/list?userId=2&mType=3").then(data => {
-          var { status, data } = data;
-          if (status === 200) {
-            this.list = data.data;
-            console.log('list', this.list)
-            this.list.reverse()
-            this.draw();
-          }
-        });
+export default {
+  data() {
+    return {
+      list: [],
+      myChart: null
+    };
+  },
+  methods: {
+    draw() {
+      console.log("abc上");
+      this.myChart = this.$echarts.init(document.getElementById("echartsDiv"));
+      console.log("abc下");
+      // console.log('myChart: ', myChart)
+      // 指定图表的配置项和数据
+      let dates = new Array();
+      let steps = new Array();
+      for (let i = 0; i < this.list.length; i++) {
+        dates.push(this.list[i].happenDate);
+        steps.push(this.list[i].reachAmount);
       }
+      console.log("dates", dates);
+      console.log("steps", steps);
+      var option = {
+        xAxis: {
+          type: "category",
+          data: dates
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: steps,
+            type: "bar"
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      this.myChart.setOption(option, true);
+      console.log("setOption(option)下");
+      console.log("option:", option);
     },
-    mounted() {
-      // this.draw();
-      this.getAllList();
+    getAllList() {
+      this.$http.get("matter_log/list?userId=2&mType=3").then(data => {
+        var { status, data } = data;
+        if (status === 200) {
+          this.list = data.data;
+          console.log("list", this.list);
+          this.list.reverse();
+          this.draw();
+        }
+      });
     }
-  };
+  },
+  mounted() {
+    // this.draw();
+    this.getAllList();
+  }
+};
 </script>
 
 <style>
